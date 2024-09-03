d39aa2bdfc0bd2781138114916555047d356da9f
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2024-09-04 02:31:36 +0300

    update app.component.html, app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CustomHeader } from '../components/custom-header/custom-header.component';
+import { Navigation } from '../components/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CustomHeader, Navigation],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
