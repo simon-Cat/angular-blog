a90d4ab521f32d52d22b36de32099eb9c91fb752
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2024-09-04 02:32:23 +0300

    update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Posts } from '../pages/posts/posts.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'posts', component: Posts }
+];
