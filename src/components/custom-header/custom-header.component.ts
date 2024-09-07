1403dfd57c4fb914d112690e0365d51952fedf39
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2024-09-07 13:38:58 +0300

    add logo link into custom-header, update styles

--- src/components/custom-header/custom-header.component.ts
+++ src/components/custom-header/custom-header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from "@angular/core";
 import { Navigation } from "../navigation/navigation.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'custom-header',
   standalone: true,
   templateUrl: './custom-header.component.html',
   styleUrl: './custom-header.component.css',
-  imports: [ Navigation ]
+  imports: [ Navigation, RouterLink ]
 })
 
 export class CustomHeader {}
