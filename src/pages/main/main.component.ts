9026e936b567fa148b6120748ea196b64f91b815
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2024-09-07 13:39:27 +0300

    add main page

---
+++ src/pages/main/main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'main',
+  standalone: true,
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+
+export class Main {}
