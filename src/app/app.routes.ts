8e5892650911daeecfa12c1f8966019218b7718f
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2024-09-07 13:40:19 +0300

    update qpp routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { Main } from '../pages/main/main.component';
 import { Posts } from '../pages/posts/posts.component'
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', component: Main },
   { path: 'posts', component: Posts }
 ];
